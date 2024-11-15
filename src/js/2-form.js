b932fa91bb2230d23ebaa8c7dbc4889a5c7fd0f8
Alina Polishchuk <alinapolishchuk2307@gmail.com>
2024-11-16 01:58:39 +0200

    task 2 form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log("Form");
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const emailField = form.elements.email;
+const messageField = form.elements.message;
+
+let formData = {
+  email: "",
+  message: "",
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+
+    emailField.value = formData.email.trim() || "";
+    messageField.value = formData.message.trim() || "";
+  }
+});
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!emailField.value.trim() || !messageField.value.trim()) {
+    alert("Fill please all fields");
+    return;
+    }
+    
+  console.log("Form data submitted:", {
+    email: emailField.value.trim(),
+    message: messageField.value.trim(),
+  });
+
+  form.reset(); 
+  localStorage.removeItem(STORAGE_KEY); 
+  formData = { email: "", message: "" }; 
+});
